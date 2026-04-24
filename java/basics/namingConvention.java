aed4a082572ea6c103b844537d4e195fa175e421
Nilesh Rathore <nileshrathore1522@gmail.com>
2026-04-24 11:51:34 +0530

    Day 2-a : Naming Convention in Java

---
+++ java/basics/namingConvention.java
@@ -0,0 +1,19 @@
+class Variables {
+    public static void main(String[] args) {
+        //Variables naming convention rules in JAVA
+        //1. Case sensitive --> use Camel Case ( snake case in python)
+        //2. can be letters, digits, numbers, dollar signs or underscores
+        //3. must begin with letters, underscore, or dollar sign
+        //4. cannot use java keywords or reserved key words
+
+        String fullName= "Nilesh Rathore";
+        int _age= 21;
+        int $Salary= 5000000;
+        String company= "Google";
+        String role= "Senior Software Developer";
+        String location= "Seattle";
+
+        System.out.println("Your name is : "+ fullName+ "and age is: "+ age+ ". Your worked at: "+ company +"for the role of : " + role+ "at :"+ location);
+    }
+    
+}
